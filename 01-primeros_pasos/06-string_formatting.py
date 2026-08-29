db5832cc26d6b269f9c12ba098e87401d04ec992
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-28 20:35:56 -0400

    feat: f-string in python, a way of format a string

---
+++ 01-primeros_pasos/06-string_formatting.py
@@ -0,0 +1,9 @@
+# Concatenación
+name = 'Ricardo'
+last_name = 'Cuéllar'
+age = 29
+# full_name = name + ' ' + last_name
+full_name = f'{name} {last_name}'
+message = f'{full_name}, tienes {age} años'
+
+print(message)  # Ricardo Cuéllar

e502fef5640fd7ce9ee672190af0c43ec6366d37
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-28 21:01:14 -0400

    feat: string indexes

---
+++ 01-primeros_pasos/07-string_indexes.py
@@ -0,0 +1,20 @@
+name = 'Ricardo'  # 6
+# print(name)  # Ricardo
+
+# print(name[0])  # R
+# print(name[1])  # i
+# ...
+# print(name[6])  # o
+
+# Luis name[3]
+
+# ¿Como obtener la ultima letra?
+# print(name[-1])
+
+# [Start:Stop]
+# Ricardo
+# Ric
+print(name[0:3])
+
+# [Start:Stop:StepOver]
+print(name[0:3:2])

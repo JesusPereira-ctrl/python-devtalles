1f57889dde21b26bdb59ace2b06935cfe65aecea
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-28 22:35:53 -0400

    feat: incorporated functions and replace method of strings

---
+++ 01-primeros_pasos/11-incorporated_functions.py
@@ -0,0 +1,17 @@
+# Funciones incorporadas - Funciones nativas
+
+print('Hola mundo')  # 'Hola Mundo'
+
+type(100)  # <class 'int'>
+
+str(100)  # '100'
+
+round(20.9)  # 21
+
+abs(-20)  # 20
+
+# Métodos - Tareas especificas
+message = 'Es un buen programador'
+new_message = message.replace('Es', 'Soy')
+
+print(new_message)

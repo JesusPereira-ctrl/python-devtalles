6a79d838ca524136289713cf18f48c70930ed204
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-29 19:01:07 -0400

    feat: ternary operator

---
+++ 02-condicionales/03-ternary_operator.py
@@ -0,0 +1,14 @@
+# if - else en una sola linea
+
+is_student = True
+
+# if is_student:
+#     print('Licencia de estudiante')
+# else:
+#     print('Licencia normal')
+
+# True if condición else False
+
+get_license = 'Licencia de estudiante' if is_student else 'Licencia normal'
+
+print(get_license)

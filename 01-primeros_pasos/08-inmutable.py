0e8955a19b36ec567558274d4c739a8124565c3b
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-28 21:18:45 -0400

    feat: inmutability, what is and how affect the strings

---
+++ 01-primeros_pasos/08-inmutable.py
@@ -0,0 +1,5 @@
+# ¿Que es inmutabilidad? Significa que algo no puede ser cambiado
+
+nombre = 'Ricardo'
+nombre[0] = 'L'  # ! Esto no se puede hacer, se debe cambiar la cadena completa
+print(nombre)

d5500ba69edf4a73bce4e299a5383c8a0a4c83ec
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-30 19:45:35 -0400

    feat: list slicing and how muting a list

---
+++ 03-listas/02-list_slicing.py
@@ -0,0 +1,18 @@
+shopping_cart = [
+    'Camisas',
+    'Tenis',
+    'Calcetas',
+    'Pantalones'
+]
+
+# [Inicio : fin]
+new_list = shopping_cart[1:3]
+
+new_list[0] = 'Zapatos'
+new_list[1] = 'Collar'
+shopping_cart[0] = 'Playeras'
+
+print(new_list)  # Crea una lista nueva
+print(shopping_cart)
+
+# Copiar una lista

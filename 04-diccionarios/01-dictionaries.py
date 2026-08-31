b3a1f76a9a64c14f565bfd2312c94e60b3d445e4
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-31 18:52:20 -0400

    feat: dictionaries

---
+++ 04-diccionarios/01-dictionaries.py
@@ -0,0 +1,17 @@
+# Diccionario = Objeto(JSON)
+# No están ordenados
+
+dictionary = {
+    'key': 'value',
+    'a': [1, 2, 3],
+    'b': 2,
+    'verdad': True
+}
+
+user = {
+    'name': 'Ricardo',
+    'country': 'México',
+    'age': 29
+}
+
+print(user['name'])

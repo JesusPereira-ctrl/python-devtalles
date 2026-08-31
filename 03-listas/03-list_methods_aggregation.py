fc850e04278a017baa26ff87d1db7382d51aeda0
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-30 20:09:00 -0400

    feat: list methods aggregation

---
+++ 03-listas/03-list_methods_aggregation.py
@@ -0,0 +1,16 @@
+# Métodos de adición
+
+numbers_list = [1, 2, 3, 4, 5]
+print(numbers_list)
+# append
+numbers_list.append(100)
+numbers_list.append(200)
+
+# insert
+numbers_list.insert(1, 200)
+numbers_list.insert(3, 300)
+
+# extend
+numbers_list.extend([11, 22, 33])
+
+print(numbers_list)

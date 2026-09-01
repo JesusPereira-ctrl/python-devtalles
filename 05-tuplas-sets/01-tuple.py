b6e05b5fc3bd34d88a1b2a84643397f0740ce3a9
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-08-31 20:17:36 -0400

    feat: tuple

---
+++ 05-tuplas-sets/01-tuple.py
@@ -0,0 +1,22 @@
+my_tuple = (1, 2, 3, 4, 'Hola', True, 2, 'Hi', 5, 4, 3, 2)
+print(my_tuple)
+
+# Ordenada
+# INMUTABLES
+# Permite duplicados
+# Indexadas
+
+# Métodos
+# .count()
+# print(my_tuple.count(2))
+
+# .index()
+# print(my_tuple.index(2))
+
+# my_tuple[4] = 'Mundo' # ESTO NO SE PUEDE
+new_tuple = my_tuple[4]
+
+print(new_tuple)
+
+week = ('Lunes', 'Martes', 'Miércoles', 'Jueves',
+        'Viernes', 'Sábado', 'Domingo')

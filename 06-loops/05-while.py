bc338c5f39b0061b83e8c65f5e19351efc6bf8d9
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-01 18:48:48 -0400

    feat: while

---
+++ 06-loops/05-while.py
@@ -0,0 +1,16 @@
+# while True:
+#     print('Esto nunca se detendrá')  # loop infinito
+
+# counter = 1
+# while counter <= 5:
+#     print(f'Number: {counter}')
+#     counter += 1
+# else:
+#     print('Terminamos')
+
+response = ''
+
+while response.lower() != 'python':
+    response = input('Escribe python para salir\n> ')
+
+print('Terminamos')

fdbdd9aab1fcff2717cc87e3ea809ab45d0b60fb
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-02 01:10:32 -0400

    feat: static method

--- 08-poo_a/07-class_static_methods.py
+++ 08-poo_a/07-class_static_methods.py
@@ -9,11 +9,20 @@ class Person:
     def change_specie(cls, new_specie):
         cls.species = new_specie
 
+    @staticmethod
+    def is_older(age):
+        return age >= 18
+
+
+# person1 = Person('Ricardo', 29)
+# print(person1.species)
+# Person.change_specie('Reptilianos')
+# print(person1.species)
+
+# person2 = Person('Fernando', 20)
+# print(person2.species)
+
+print(Person.is_older(16))
 
 person1 = Person('Ricardo', 29)
-print(person1.species)
-Person.change_specie('Reptilianos')
-print(person1.species)
-
-person2 = Person('Fernando', 20)
-print(person2.species)
+print(person1.change_specie(person1.age))

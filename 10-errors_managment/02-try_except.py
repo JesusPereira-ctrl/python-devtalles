e840ecc2fa2f2579ed5912c9c85b8f1c40f15346
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-02 16:48:09 -0400

    feat: try and except

---
+++ 10-errors_managment/02-try_except.py
@@ -0,0 +1,20 @@
+def divide_numbers():
+    try:
+        a = int(input('Ingresa el numerador\n> '))  # a / b
+        b = int(input('Ingresa el denominador\n> '))
+
+        result = a / b
+        print(result)
+        return result
+    except ValueError:
+        print('Por favor, ingresa solo números.')
+    except ZeroDivisionError:
+        print('No se puede dividir entre cero.')
+    except Exception as error:
+        print(type(error))
+
+
+divide_numbers()
+# try
+
+# except
